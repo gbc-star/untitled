3aaf5679eef8efca279852ac79f3a1c3bf9fc744
Bochuan Gao <1295670385@qq.com>
2023-01-08 19:42:09 +0800

    degree

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a;
+        a = 37;
+        int b = (int) (a * 1.8 + 32);
+        System.out.println(b);
+        System.out.println("Hello world!");
+    }
+}
